841ca6161a276c2cf26a3112db33a6e263c6e963
andrewhsu <382272420@qq.com>
2018-09-14 18:39:29 +0800

    数据库

--- application/config/database_dev.php
+++ application/config/database_dev.php
@@ -1,11 +1,11 @@
 <?php
 return [
     // 服务器地址
-    'hostname' => '127.0.0.1',
+    'hostname' => '47.75.181.113',
     // 数据库名
-    'database' => 'shudao',
+    'database' => 'mfish',
     // 用户名
-    'username' => 'root',
+    'username' => 'admin',
     // 密码
-    'password' => 'root',
+    'password' => 'ynJmh1R(!vmIemN',
 ];
